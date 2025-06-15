e289094119fe8a23cf67e34f263d90c8bea519e8
backboss@daum.net <info3821!@#$>
2025-06-15 15:03:05 +0900

    main  - log Handler  - ip 변경

--- log_handler/airflow_local_settings.py
+++ log_handler/airflow_local_settings.py
@@ -6,14 +6,20 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 from plugins.log_handler.http_log_handler import FileAndHttpLogHandler
 
 
+def get_file_and_http_log_handler():
+    return FileAndHttpLogHandler(
+        base_log_folder='/opt/airflow/logs',
+        filename_template='{{ dag_id }}/{{ task_id }}/{{ execution_date }}/{{ try_number }}.log'
+    )
+
 
 LOGGING_CONFIG = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
         'task': {
-            'class': 'plugins.log_handler.http_log_handler.FileAndHttpLogHandler',
-            'formatter': 'airflow'
+            'class': 'logging.StreamHandler',  # dummy, will be replaced by factory
+            'formatter': 'airflow',
         },
     },
     'loggers': {
@@ -29,3 +35,9 @@ LOGGING_CONFIG = {
         },
     },
 }
+
+# Override handler after dictConfig is loaded
+import logging.config
+logging.config._handlerList.clear()
+logging.getLogger('airflow.task').handlers.clear()
+logging.getLogger('airflow.task').addHandler(get_file_and_http_log_handler())
