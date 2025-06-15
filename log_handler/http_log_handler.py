de35f7fda70f8ca0d018db7ece3a2aa77cf7b497
backboss@daum.net <info3821!@#$>
2025-06-15 14:53:46 +0900

    main  - log Handler  - ip 변경

--- log_handler/http_log_handler.py
+++ log_handler/http_log_handler.py
@@ -1,5 +1,3 @@
-# plugins/log_handler/http_log_handler.py
-
 import logging
 import requests
 from airflow.utils.log.file_task_handler import FileTaskHandler
@@ -11,27 +9,29 @@ class HttpLogHandler(logging.Handler):
         self.formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
     def emit(self, record):
-
-        print(f"111111111111111111111111111111111111")
+        # 로그 확인용
         with open("/tmp/emit_called1.log", "a") as f:
-            f.write("✅ emit() called\n")
+            f.write("✅ HttpLogHandler emit() called\n")
 
         try:
             log_entry = self.format(record)
             requests.post(self.url, json={"log": log_entry}, timeout=1.0)
         except Exception as e:
             with open("/tmp/http_send_error.log", "a") as f:
-                f.write(str(e) + "\n")
+                f.write(f"{str(e)}\n")
 
 class FileAndHttpLogHandler(FileTaskHandler):
-    def __init__(self, base_log_folder, filename_template):
-        super().__init__(base_log_folder, filename_template)
+    def __init__(self, *args, **kwargs):
+        # FileTaskHandler expects base_log_folder and filename_template in args
+        super().__init__(*args, **kwargs)
         self.http_handler = HttpLogHandler()
 
     def emit(self, record):
-        print(f"000000000000000000000000000000000000000000000000000000000000000")
+
+        print(f"111111111111111111111111111111111111111111111111111111111111111111")
+
         with open("/tmp/emit_called.log", "a") as f:
-            f.write("✅ emit() called\n")
+            f.write("✅ FileAndHttpLogHandler emit() called\n")
 
         super().emit(record)
         self.http_handler.emit(record)
