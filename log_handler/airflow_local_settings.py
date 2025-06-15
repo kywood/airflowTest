7281ba46535cdb6a34192d9b2faaa6f93a8173df
backboss@daum.net <info3821!@#$>
2025-06-15 14:28:12 +0900

    main  - log Handler  - ip 변경

---
+++ log_handler/airflow_local_settings.py
@@ -0,0 +1,24 @@
+from plugins.log_handler.http_log_handler import FileAndHttpLogHandler
+
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'task': {
+            'class': 'plugins.log_handler.http_log_handler.FileAndHttpLogHandler',
+            'formatter': 'airflow',
+        },
+    },
+    'loggers': {
+        'airflow.task': {
+            'handlers': ['task'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+    'formatters': {
+        'airflow': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        },
+    },
+}
